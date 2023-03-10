a1bf8ec4d446c1a3673cb5cadb85044a13636507
nidhiG30 <gadgenidhi9@gmail.com>
2023-03-10 22:46:45 +0530

    Typo fixed.

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Profile from './components/Profile';
 import Shimmer from './components/Shimmer';
 import UserContext from './utils/UserContext';
 import { Provider } from 'react-redux';
-import store from './utils/Store';
+import store from './utils/store';
 import Cart from './components/Cart';
 
 const Instamart = lazy(() => import('./components/Instamart'));
@@ -90,10 +90,8 @@ const appRouter = createBrowserRouter([
       },
       {
         path: '/cart',
-        element: (
-          <Cart/>
-        )
-      }
+        element: <Cart />,
+      },
     ],
   },
 ]);
